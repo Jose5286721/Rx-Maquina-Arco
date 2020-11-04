31fb3067fbfc7da2a3978f516530b1188bf12ba2
jose-arco <jose-arco@MacBook-Pro-2.local>
2020-11-04 14:27:53 -0300

    1.1

--- src/main/java/py/com/arco/App.java
+++ src/main/java/py/com/arco/App.java
@@ -1,18 +1,16 @@
 package py.com.arco;
 
+
 import com.pi4j.io.gpio.*;
 
 public class App {
+    private static GpioPinDigitalOutput pin;
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hola a todos");
-        GpioController gpioController = GpioFactory.getInstance();
-        GpioPinDigitalOutput pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01,"LED",PinState.HIGH);
-        pin.setShutdownOptions(true,PinState.LOW);
-        while(true){
-            Thread.sleep(100);
-            pin.low();
-            Thread.sleep(100);
-            pin.high();
+        if(pin==null){
+            GpioController gpioController = GpioFactory.getInstance();
+            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05,"LED", PinState.LOW);
         }
+        pin.toggle();
     }
 }
