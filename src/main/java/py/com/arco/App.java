92c6ce85fe2a815512b0fd0a9354095676c637d1
jose-arco <jose-arco@MacBook-Pro-2.local>
2020-11-04 16:38:47 -0300

    cambios

--- src/main/java/py/com/arco/App.java
+++ src/main/java/py/com/arco/App.java
@@ -1,19 +1,37 @@
 package py.com.arco;
 
-
 import com.pi4j.io.gpio.*;
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.ChannelEventListener;
+import com.pusher.client.channel.PusherEvent;
+import com.pusher.client.channel.SubscriptionEventListener;
 
 public class App {
     private static GpioPinDigitalOutput pin;
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hola a todos");
-        if(pin==null){
-            GpioController gpioController = GpioFactory.getInstance();
-            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05,"LED", PinState.LOW);
-        }
-        while (true){
-            pin.toggle();
-            Thread.sleep(200);
-        }
+    public static void main(String[] args){
+            if(pin == null){
+                GpioController gpioController = GpioFactory.getInstance();
+                pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05,"led", PinState.LOW);
+            }
+            PusherOptions pusherOptions = new PusherOptions();
+            pusherOptions.setCluster("us2");
+            Pusher pusher = new Pusher("eaec0efbd968f46ba3f8", pusherOptions);
+            pusher.connect();
+            Channel channel = pusher.subscribe("prueba");
+            channel.bind("evento", new SubscriptionEventListener() {
+                @Override
+                public void onEvent(PusherEvent event) {
+                    pin.toggle();
+                }
+            });
+            while (true){
+                try{
+                    Thread.sleep(200);
+                }catch (InterruptedException ex){
+                    ex.printStackTrace();
+                }
+            }
     }
 }
