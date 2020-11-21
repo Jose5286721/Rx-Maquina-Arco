e25f8c6556a40391530975e7810aed0ac83b3d59
jose-arco <joseascurra123@gmail.com>
2020-11-21 11:00:34 -0300

    Cambios

--- src/main/java/py/com/arco/App.java
+++ src/main/java/py/com/arco/App.java
@@ -3,35 +3,53 @@ package py.com.arco;
 import com.pi4j.io.gpio.*;
 import com.pusher.client.Pusher;
 import com.pusher.client.PusherOptions;
-import com.pusher.client.channel.Channel;
-import com.pusher.client.channel.ChannelEventListener;
-import com.pusher.client.channel.PusherEvent;
-import com.pusher.client.channel.SubscriptionEventListener;
+import com.pusher.client.channel.*;
 
 public class App {
-    private static GpioPinDigitalOutput pin;
+    //private static GpioPinDigitalOutput pin;
     public static void main(String[] args){
-            if(pin == null){
-                GpioController gpioController = GpioFactory.getInstance();
-                pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05,"led", PinState.LOW);
-            }
+            //if(pin == null){
+            //    GpioController gpioController = GpioFactory.getInstance();
+            //    pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_02,"led", PinState.LOW);
+            //}
             PusherOptions pusherOptions = new PusherOptions();
             pusherOptions.setCluster("us2");
             Pusher pusher = new Pusher("eaec0efbd968f46ba3f8", pusherOptions);
             pusher.connect();
-            Channel channel = pusher.subscribe("prueba");
-            channel.bind("evento", new SubscriptionEventListener() {
+            Channel channel = pusher.subscribe("sucursales.2");
+            channel.bind("App\\Events\\HabilitarMaquinaSucursal", new SubscriptionEventListener() {
                 @Override
                 public void onEvent(PusherEvent event) {
-                    pin.toggle();
+                    //pin.high();
+                    System.out.println(event.getData());
+                    //detenerMaquina();
                 }
             });
-            while (true){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(true){
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
+    }
+    private static void detenerMaquina(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
                 try{
-                    Thread.sleep(200);
+                    Thread.sleep(30*1000);
+                    //pin.low();
                 }catch (InterruptedException ex){
                     ex.printStackTrace();
                 }
             }
+        });
     }
 }
